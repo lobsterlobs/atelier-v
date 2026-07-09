23c1546b6567e9adb7c74dcb31f25d1de7aff9c9
lobsterlobs <lobsterlobs@gmail.com>
2026-07-09 10:00:50 +0300

    Update sw.js

--- smart-card/sw.js
+++ smart-card/sw.js
@@ -1,4 +1,36 @@
-const CACHE = 'winners-card-v2';
-const ASSETS = ['./','./index.html','./style.css','./script.js','./manifest.json','./contact.vcf','./icon-192.png','./icon-512.png','./logo-crop.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'winners-card-v3';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './contact.vcf',
+  './icon-192.png',
+  './icon-512.png',
+  './logo-crop.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE) return caches.delete(key);
+      }))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
